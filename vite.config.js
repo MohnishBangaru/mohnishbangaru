0d788f23af7495c6c4cf62a89d17551aa59c829f
MohnishBangaru <mb9628@nyu.edu>
2025-04-15 03:23:03 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mohnishbangaru.github.io", // super important!,
+  base: "/mohnishbangaru", // super important!,
 })
